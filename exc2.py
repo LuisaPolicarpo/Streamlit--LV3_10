af8260282d85e112be216f356e287671730dea0e
Luísa Colaço de Freitas <108865217+LuisaPolicarpo@users.noreply.github.com>
2022-10-03 12:34:52 +0100

    first commit

---
+++ exc2.py
@@ -0,0 +1,29 @@
+
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+
+
+
+st.title("Hello class")
+
+
+add_selectbox = st.sidebar.radio(
+    "How would you like to be contacted?",
+    ("Sales", "Finance", "HR"))
+
+if add_selectbox=='Sales':
+    st.markdown ('''Welcome to *Sales*''')
+elif add_selectbox == 'HR':
+    st.markdown('''Hi_this_is_*HR*''')
+
+else: 
+    st.markdown('''Heloooo''')
+    
+ 
+[theme]
+primaryColor="#F63366"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#F0F2F6"
+textColor="#262730"
+font="sans serif"
